f8d28485b0bb817ebfd88d690ab469877d7c56ca
femiaghedo <aghedojoe007@gmail.com>
2019-05-16 10:15:10 +0100

    using index.py to run all methods

--- index.py
+++ index.py
@@ -1,8 +1,12 @@
-import createUser as acccountcreate
+import createUser as createUser
 import loginUser as loginUser
 
 
 # for num in range(0,1): 
 #     loginUser.loginUser()
 
-#loginUser.loginUser()
+#uncomment below to run user login
+# loginUser.loginUser() 
+
+#uncomment below to run user accounnt creation
+#createUser.createAccount() 
